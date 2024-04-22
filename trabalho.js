89b70665b260cd26a4e2776cbd9428d0bac93bf4
Rhayr <rhayra.fiorentin@gmail.com>
2024-04-22 00:52:41 -0300

    fisrt commit

---
+++ trabalho.js
@@ -0,0 +1,39 @@
+const listaPessoas = [];
+
+function gerarId(){
+    return Math.floor(Math.random() * 100);  
+}
+
+function cadastrarPessoa(){
+    const nome = prompt("Digite o nome: ");
+    const idade = prompt("Digite a idade: ");
+    const cidade = prompt("Digite a cidade: ");
+    const curso = prompt("Digite o curso: ")
+
+    const pessoa = {nome, idade, cidade, curso};
+    pessoa.id = gerarId();
+
+    console.log("Pessoa cadastrada: ", pessoa)
+
+    listaPessoas.push(pessoa);
+}
+
+function atualizarPessoa() {
+    const id = prompt("Digite o Id da pessoa que deseja atualizar: ")
+    const pessoa = listaPessoas.find(pessoa => pessoa.id === parseInt(id, 10));
+
+    if(pessoa) {
+
+        const dadosAtualizados = {}
+
+        dadosAtualizados.nome = prompt("Digite o nome atualizado: ");
+        dadosAtualizados.idade = prompt("Digite a idade atualizada: ");
+        dadosAtualizados.cidade = prompt("Digite a cidade atualizada: ");
+        dadosAtualizados.curso = prompt("Digite o curso atualizado: ");
+    
+        Object.assign(pessoa, dadosAtualizados);
+        console.log("Pessoa atualizada: ", pessoa);
+    } else {
+        console.log("Pessoa não cadastrada no sistema.")
+    }
+}
